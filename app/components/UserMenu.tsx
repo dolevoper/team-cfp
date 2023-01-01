dab5de3a3a45569c8306c9ee14735bd448300480
Omer Dolev <odolev@microsoft.com>
2023-01-01 23:57:46 +0200

    use details for user menu

--- app/components/UserMenu.tsx
+++ app/components/UserMenu.tsx
@@ -1,72 +1,73 @@
 import { Link } from "@remix-run/react";
 import { useId, useRef } from "react";
-import { useToggle } from "~/utils/hooks";
 import type { UserData } from "~/utils/session.server";
 import { Persona } from "./Persona";
 
 type UserMenuProps = { user: UserData };
 export function UserMenu({ user }: UserMenuProps) {
-  const [isOpen, toggleIsOpen] = useToggle();
-  const buttonRef = useRef<HTMLButtonElement>(null);
+  const detailsRef = useRef<HTMLDetailsElement>(null);
+  const summaryRef = useRef<HTMLElement>(null);
   const signoutRef = useRef<HTMLAnchorElement>(null);
   const id = useId();
 
-  const buttonId = `${id}-button`;
+  const summaryId = `${id}-button`;
   const menuId = `${id}-menu`;
 
   return (
-    <div data-user-menu>
-      <button
+    <details
+      data-user-menu
+      ref={detailsRef}
+      onToggle={(e) => {
+        e.currentTarget.open && signoutRef.current?.focus();
+      }}
+    >
+      <summary
         aria-haspopup="menu"
-        aria-expanded={isOpen ? true : undefined}
+        aria-expanded={detailsRef.current?.open ? true : undefined}
         aria-controls={menuId}
-        id={buttonId}
-        ref={buttonRef}
+        id={summaryId}
+        ref={summaryRef}
         title={`${user.name} (${user.preferred_username})`}
-        onClick={() => {
-          toggleIsOpen();
-          !isOpen && setTimeout(() => signoutRef.current?.focus());
-        }}
       >
         <span data-visually-hidden>User menu</span>
         <Persona userData={user} size="1.2rem" initialsOnly />
-      </button>
+      </summary>
       <menu
-        aria-labelledby={buttonId}
-        tabIndex={isOpen ? 0 : undefined}
+        aria-labelledby={summaryId}
+        tabIndex={0}
         id={menuId}
         onBlur={(e) => {
           if (
-            !isOpen ||
+            !detailsRef.current?.open ||
             e.currentTarget.contains(e.relatedTarget) ||
-            e.relatedTarget === buttonRef.current
+            e.relatedTarget === summaryRef.current
           ) {
             return;
           }
 
-          toggleIsOpen();
+          if (detailsRef.current) {
+            detailsRef.current.open = false;
+          }
         }}
         onKeyDown={(e) => {
           if (e.key !== "Tab" && e.key !== "Escape") {
             return;
           }
 
-          buttonRef.current?.focus();
-          toggleIsOpen();
+          summaryRef.current?.focus();
+          if (detailsRef.current) {
+            detailsRef.current.open = false;
+          }
         }}
       >
-        <li role="menuitem">
-          <hgroup>
-            <h2>{user.name}</h2>
-            <p>{user.preferred_username}</p>
-          </hgroup>
-        </li>
-        <li role="menuitem">
-          <Link to="/.auth/logout" ref={signoutRef} tabIndex={!isOpen ? -1 : undefined}>
-            Sign out
-          </Link>
-        </li>
+        <hgroup role="menuitem">
+          <h2>{user.name}</h2>
+          <p>{user.preferred_username}</p>
+        </hgroup>
+        <Link to="/.auth/logout" ref={signoutRef} role="menuitem">
+          Sign out
+        </Link>
       </menu>
-    </div>
+    </details>
   );
 }
